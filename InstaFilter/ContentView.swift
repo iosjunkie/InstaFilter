34d2e6a45e034466b869d41b080ded5814a14e8e
Jules Lee <jackconsolidated@gmail.com>
2020-01-12 18:54:32 +0800

    Initial Commit

---
+++ InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by Jules Lee on 1/12/20.
+//  Copyright © 2020 Jules Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
